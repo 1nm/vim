ef728535e410add4436b72b6a4834d1f37971406
1nm <1nm@noreply.github.com>
2019-01-08 12:35:27 +0900

    No plugin manager

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-cp .vimrc-plugin-install ~/.vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && \
-vim +PluginInstall +qall
+git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox
 cp .vimrc ~/.vimrc
