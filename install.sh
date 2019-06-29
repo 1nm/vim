13d52be2c86e0a2b35b7b8e029c8af97cf333438
1nm <1nm@users.noreply.github.com>
2019-06-29 19:20:50 +0900

    Update tmux conf

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf
